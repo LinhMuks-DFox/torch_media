49147643c310f757bf0d659247742029bdedeaf3
Mux <muxxum65536@gmail.com>
2025-01-17 18:03:42 +0900

    fix

--- libtorchmedia/xmake.lua
+++ libtorchmedia/xmake.lua
@@ -1,11 +1,9 @@
 target("torch_media")
     set_kind("headeronly")
     add_headerfiles("include/(**.hpp)")
-    add_includedirs("include", {public = true}) -- 公开头文件路径
+    add_includedirs("include", {public = true})
     set_basename("torch_media")
     set_license("MIT")
-
-    -- 动态添加用户提供的依赖
     on_load(function (target)
         if has_package("fmt") then
             target:add("packages", "fmt")
